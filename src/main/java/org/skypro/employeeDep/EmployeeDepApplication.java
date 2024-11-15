ea7e250405459625b39d57791941d747245728f9
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-11-15 23:41:34 +0300

    начальный коммит

---
+++ src/main/java/org/skypro/employeeDep/EmployeeDepApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.employeeDep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDepApplication.class, args);
+	}
+
+}
